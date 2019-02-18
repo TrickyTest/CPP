ed690dd7e1768f13e3935a5d3aa216c3508a29f0
Tricky1975 <facebook@tbbs.nl>
2019-02-18 11:01:32 +0100

    Huh?

---
+++ 0007_arrays/huh.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+int main()
+{
+    int scores[] = { 84, 92, 76, 81, 56 };
+//  const int numStudents = std::size(scores); // requires C++17 and <iterator> header
+    const int numStudents = sizeof(scores) / sizeof(scores[0]); // use this instead if not C++17 capable
+ 
+    int maxScore = 0; // keep track of our largest score
+    for (int student = 0; student < numStudents; ++student)
+        if (scores[student] > maxScore)
+            maxScore = scores[student];
+ 
+    std::cout << "The best score was " << maxScore << '\n';
+ 
+    return 0;
+}
